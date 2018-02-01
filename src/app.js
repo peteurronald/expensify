b54aee4370da4a9ed1797ee268abc90b717257ac
Peter Ronald <peter.ronald@live.co.uk>
2018-02-01 11:34:52 +0000

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ import './styles/styles.scss';
 import { setTimeout } from 'timers';
 
 const store = configureStore();
+console.log("test");
 // store.dispatch(addExpense({description:'Water bill', amount:4500}));
 // store.dispatch(addExpense({description:'Gas bill', createdAt: 1000}));
 // store.dispatch(addExpense({description:'Rent', amount:109500}));
