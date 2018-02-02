7d12d0b065e3e271b888be34550362e13106df23
Peter Ronald <peter.ronald@live.co.uk>
2018-02-02 11:31:05 +0000

    Setup expense summary page

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -5,13 +5,12 @@ import numeral from 'numeral';
 
 
 export default ({id, description, amount, createdAt}) => (
-    
     <div>
     <Link to={`/edit/${id}`}>
         <h3>{description}</h3>
     </Link>
         <p>
-        {numeral(amount / 100).format('0,0.00')} 
+        {numeral(amount / 100).format('$0,0.00')} 
         - 
         {moment(createdAt).format('MMMM  Do, YYYY')} </p>
     </div>
