8d436c79273ccc3d15fd62eedf31b1ad7d754b77
Peter Ronald <peter.ronald@live.co.uk>
2018-02-13 17:24:42 +0000

    Setup set Expenses

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -46,3 +46,14 @@ test('should not edit an expense if not found', ()=> {
     const state = expensesReducer(expenses, action);
     expect(state).toEqual(expenses);  
 });
+
+
+test('should set expenses',() => {
+    const action = {
+        type: 'SET_EXPENSES',
+        expenses: [expenses[0]]
+    }; 
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([expenses[0]]);
+});
+
